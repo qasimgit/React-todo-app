c541f52003aaede60e8861f9298d084816a1be90
qasimgit <qasimpkkhi@gmail.com>
2020-06-27 14:34:33 +0500

    setup created for developing todo app

---
+++ src/components/header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export const Header = () => {
+
+    return (
+        <div className='container'>
+            <div>
+                <h1>
+                    React Todo App
+                </h1>
+                <h4>
+                    By Muhammad Qasim Qadri
+                </h4>
+            </div>
+
+        </div>
+    )
+}
