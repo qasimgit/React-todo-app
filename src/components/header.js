d58436c0cdf599206ea577d04d7903c15693194b
qasimgit <qasimpkkhi@gmail.com>
2020-07-02 22:04:50 +0500

    Add - Title Changed

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ export const Header = () => {
   return (
     <div>
       <div>
-        <h1>React Todo App</h1>
+        <h1>React Todo App (Master)</h1>
         <h4>By Muhammad Ahmed Raza Qadri</h4>
       </div>
     </div>
